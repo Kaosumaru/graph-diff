242953a1e746bd4de5601eae291e2a3b5f230afe
Kaosumaru <mateuszborycki@gmail.com>
2024-09-22 00:41:39 +0200

    Small improvement.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css';
 import { useRete } from 'rete-react-plugin';
 import { createEditor } from './editor';
+import { SampleGraph, SampleGraph2 } from './interface/SampleData';
+import { DiffGraph } from './logic/DiffGraph';
+
+const graph1 = SampleGraph();
+const graph2 = SampleGraph2();
+
+const diff = DiffGraph(graph1, graph2);
+const graphView = createEditor(diff);
 
 function App() {
-  const [ref] = useRete(createEditor);
+
+  
+  const [ref] = useRete(graphView);
   return (
     <div className="App">
       <div ref={ref} style={{ height: "100vh", width: "100vw" }}></div>
