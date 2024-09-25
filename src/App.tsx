81fc8b4282edcfcf9f6c6cfb89cd6f8c8e4f86fc
Kaosumaru <mateuszborycki@gmail.com>
2024-09-25 22:03:29 +0200

    Small update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,12 @@
 import './App.css';
-import styled from 'styled-components';
 import { ComparisionComponent } from './ui/ComparisionComponent';
-import { Area } from './ui/Area';
-import ReactJsonViewCompare from 'react-json-view-compare';
 
-const Layout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 0.3fr;
-  grid-template-rows: 2fr 3fr;
-  grid-template-areas:
-    'canvas result'
-    'canvas result';
-  gap: 0.6em;
-  padding: 0.6em;
-  box-sizing: border-box;
-  overflow: hidden;
-`
 
-const Source = styled(Area)`
-  grid-area: source;
-  position: relative;
-`
-
-const Result = styled(Area)`
-  grid-area: result;
-  position: relative;
-`
-
-const Canvas = styled(Area)`
-  grid-area: canvas;
-  position: relative;
-`
-
-const oldData = {
-  name: 'super',
-  age: 18,
-  task: [
-    { name: 'eat', time: '09:00' },
-    { name: 'work', time: '10:00' },
-    { name: 'sleep', time: '22:00' }
-  ]
-};
-const newData = {
-  name: 'coolapt',
-  age: 20,
-  task: [
-    { name: 'eat', time: '09:00' },
-    { name: 'work', time: '10:00' },
-    { name: 'sleep', time: '23:00' },
-    { name: 'running', time: '08:00' }
-  ]
-};
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-
-        <Result>
-         <ReactJsonViewCompare oldData={oldData} newData={newData} />
-        </Result>
-        <Canvas>
-          <ComparisionComponent/>
-        </Canvas>
-      </Layout>
-      
+      <ComparisionComponent/>    
     </div>
   );
 }
--- src/ui/ComparisionComponent.tsx
+++ src/ui/ComparisionComponent.tsx
@@ -2,6 +2,53 @@ import { useRete } from 'rete-react-plugin';
 import { SampleGraph, SampleGraph2 } from '../interface/SampleData';
 import { DiffGraph } from '../logic/DiffGraph';
 import { createEditor } from './editor';
+import ReactJsonViewCompare from 'react-json-view-compare';
+import styled from 'styled-components';
+import { Area } from './Area';
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 0.3fr;
+  grid-template-rows: 2fr 3fr;
+  grid-template-areas:
+    'canvas result'
+    'canvas result';
+  gap: 0.6em;
+  padding: 0.6em;
+  box-sizing: border-box;
+  overflow: hidden;
+`
+
+const Result = styled(Area)`
+  grid-area: result;
+  position: relative;
+  overflow-y:scroll;
+`
+
+const Canvas = styled(Area)`
+  grid-area: canvas;
+  position: relative;
+`
+
+const oldData = {
+  name: 'super',
+  age: 18,
+  task: [
+    { name: 'eat', time: '09:00' },
+    { name: 'work', time: '10:00' },
+    { name: 'sleep', time: '22:00' }
+  ]
+};
+const newData = {
+  name: 'coolapt',
+  age: 20,
+  task: [
+    { name: 'eat', time: '09:00' },
+    { name: 'work', time: '10:00' },
+    { name: 'sleep', time: '23:00' },
+    { name: 'running', time: '08:00' }
+  ]
+};
 
 const graph1 = SampleGraph();
 const graph2 = SampleGraph2();
@@ -9,10 +56,18 @@ const graph2 = SampleGraph2();
 const diff = DiffGraph(graph1, graph2);
 const graphView = createEditor(diff);
 
+
 export function ComparisionComponent() {
     const [ref] = useRete(graphView);
   
     return (
-        <div ref={ref} style={{ height: "100vh", width: "100vw" }}></div>
+    <Layout>
+        <Result>
+            <ReactJsonViewCompare oldData={oldData} newData={newData} />
+        </Result>
+        <Canvas>
+            <div ref={ref} style={{ height: "100vh", width: "100vw" }}></div>
+        </Canvas>
+    </Layout>
     );
 }
