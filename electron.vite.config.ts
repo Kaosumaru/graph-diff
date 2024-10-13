9205adfd6172300abd2dffdb048f12d8f48c1da7
Mateusz Borycki <mateuszborycki@gmail.com>
2024-10-13 20:59:53 +0200

    Added missing aliases.

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
     renderer: {
         resolve: {
             alias: {
-                '@renderer': resolve('src/renderer/src')
+                '@renderer': resolve('src/renderer/src'),
+                '@diff': resolve('src/diff'),
+                '@components': resolve('src/components')
             }
         },
         plugins: [react()]
